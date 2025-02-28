6d304e21312767c65e6df750e519cd5da75d961c
Vinicius Prado <developer.vinicius.prado@gmail.com>
2025-02-28 18:32:51 -0300

    feature: Manipulação global de exceptions
    - implemente o controle global de exceptions;

---
+++ src/main/java/com/merlin/backendefienciente/application/RestControlAdviceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.merlin.backendefienciente.application;
+
+import com.merlin.backendefienciente.domain.exceptions.ClienteNaoEncontradoException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class RestControlAdviceExceptionHandler {
+
+    private static final Logger log = LogManager.getLogger(RestControlAdviceExceptionHandler.class);
+
+    @ExceptionHandler(ClienteNaoEncontradoException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String clienteNaoEncontradoException(ClienteNaoEncontradoException ex, WebRequest request){
+        String clienteNaoEncontrado = "Cliente não encontrado!";
+        log.error(clienteNaoEncontrado, ex);
+        return clienteNaoEncontrado;
+    }
+}
