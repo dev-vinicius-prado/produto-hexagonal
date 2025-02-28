0c1c8749d5ccdb434466bbc674ad541957e0405d
Vinicius Prado <developer.vinicius.prado@gmail.com>
2025-02-27 21:52:39 -0300

    feature: Visibilidade
    - corrige a visibilidade das interfaces;

--- src/main/java/com/merlin/backendefienciente/infrastructure/cliente/ClienteSpringRepository.java
+++ src/main/java/com/merlin/backendefienciente/infrastructure/cliente/ClienteSpringRepository.java
@@ -3,6 +3,6 @@ package com.merlin.backendefienciente.infrastructure.cliente;
 import com.merlin.backendefienciente.domain.cliente.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteSpringRepository extends JpaRepository<Cliente, Long> {
+interface ClienteSpringRepository extends JpaRepository<Cliente, Long> {
     Cliente findByCpf(String cpf);
 }
--- src/main/java/com/merlin/backendefienciente/infrastructure/produto/ProdutoSpringRepository.java
+++ src/main/java/com/merlin/backendefienciente/infrastructure/produto/ProdutoSpringRepository.java
@@ -3,5 +3,5 @@ package com.merlin.backendefienciente.infrastructure.produto;
 import com.merlin.backendefienciente.domain.produto.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoSpringRepository extends JpaRepository<Produto, Long> {
+interface ProdutoSpringRepository extends JpaRepository<Produto, Long> {
 }
